8193af9508c82b86446812bc8a11debc61b140e9
Daniel Flanagan <37907774+FlantasticDan@users.noreply.github.com>
2021-08-13 14:03:44 -0700

    Friendly Viewer

--- static/feed.js
+++ static/feed.js
@@ -1,5 +1,11 @@
 const socket = io()
 
+const content = document.getElementById('content')
+
 socket.on('update', payload => {
-    console.log(payload)
+    html = ''
+    Object.entries(payload).forEach(item => {
+        html += `<h3>${item[0]}</h3><h1>${item[1]}</h1>`
+    })
+    content.innerHTML = html
 })
